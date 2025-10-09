3154a96121563004e76e2faca7346e61c7986aa5
Miguelilloo <miguelenyedi@gmail.com>
2025-10-09 13:06:23 +0200

    Versión inicial del proyecto con Tailwind configurado

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{html,js,php}",
+    "./components/**/*.{html,js}",
+    "./js/**/*.{js,ts}",
+    "./*.html"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
